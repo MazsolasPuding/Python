aef3baaf10e374b863435cf9203cd0af51857680
Horváth Dávid <80383612+MazsolasPuding@users.noreply.github.com>
2022-09-26 18:49:39 +0200

    'Added Python template file to root.'

---
+++ template.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+
+class Template:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    Template()
