4df6cfb57343c04d7b211011d6ae87f6d0773962
Dávid Horváth <horvath.david35@gmail.com>
2022-09-30 18:47:14 +0200

    picture and video read

--- OpenCV/Basics/read.py
+++ OpenCV/Basics/read.py
@@ -4,4 +4,16 @@ img = cv.imread('C:/Users/horva/Pictures/canvas.png')
 
 cv.imshow('Cat', img)
 
+capture = cv.VideoCapture('D:/Torrent/Mamma.Mia!/Mamma.Mia.avi')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) and 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
 cv.waitKey(0)
